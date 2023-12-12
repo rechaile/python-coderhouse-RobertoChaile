246f11490e5239038380583ca736944465ded217
Roberto Chaile <rechaile@gmail.com>
2023-12-12 20:43:55 -0300

    agrego cambios de las clases

--- Modulos/cliente.py
+++ Modulos/cliente.py
@@ -17,22 +17,38 @@ class Cliente:
 
     # Método para comprar un producto
     def comprar(self, producto):
+
+        # Compra el producto
         self.lista_de_compras.append(producto)
-        print(f"El cliente {self.nombre} ha comprado el producto {producto}")
+
+        # Devuelve la información del producto
+        return f"El cliente {self.nombre} ha comprado el producto {producto.nombre}."
 
     # Método para agregar un producto a favoritos
     def agregar_a_favoritos(self, producto):
         self.lista_de_favoritos.append(producto)
-        print(f"El cliente {self.nombre} ha agregado el producto {producto} a sus favoritos")
+        print(f"El cliente {self.nombre} ha agregado el producto {producto.nombre} a sus favoritos")
 
     # Método para mostrar la lista de compras
     def mostrar_lista_de_compras(self):
         print(f"La lista de compras del cliente {self.nombre} es:")
         for producto in self.lista_de_compras:
-            print(f"* {producto}")
+            print(f"* {producto.nombre}")
 
     # Método para mostrar la lista de favoritos
     def mostrar_lista_de_favoritos(self):
         print(f"La lista de favoritos del cliente {self.nombre} es:")
         for producto in self.lista_de_favoritos:
-            print(f"* {producto}")
+            print(f"* {producto.nombre}")
+            
+    # Método para calcular el total a pagar
+    def calcular_total(self):
+        total = 0
+        for producto in self.lista_de_compras:
+            total += producto.precio
+        return total
+    
+    # Método para mostrar el total a pagar
+    def mostrar_total(self):
+        total = self.calcular_total()
+        print(f"El total a pagar es: {total}")
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-import producto
-import cliente
+from Modulos import producto as product
+from Modulos import cliente
 
 
-def main():
-    # Creación de un cliente
-    cliente = cliente.Cliente("Juan", "Pérez", "juan.perez@gmail.com", "123456789")
 
-    # Creación de un producto
-    producto = producto.Producto("Televisor 55 pulgadas", "Televisor LED de 55 pulgadas con resolución 4K", 10000)
+# Creación de un cliente
+cliente = cliente.Cliente("Juan", "Pérez", "juan.perez@gmail.com", "123456789")
 
-    # Compra del producto
-    cliente.comprar(producto)
+# Creación de un producto
+producto = product.Producto("Televisor 55 pulgadas", "Televisor LED de 55 pulgadas con resolución 4K", 10000)
+producto2 = product.Producto("iPhone 15", "Celular iPhone 15 128Gb Camara 8Mpx", 1000)
+producto3 = product.Producto("Lavarropas Whirlpool", "Lavarropas de 30lts con velocidad de centrifugado de 1000rpm", 55000)
+# Compra del producto
+cliente.comprar(producto)
+cliente.comprar(producto2)
+cliente.agregar_a_favoritos(producto3)
 
-
-if __name__ == "__main__":
-    main()
+cliente.mostrar_lista_de_compras()
+cliente.mostrar_lista_de_favoritos()
+cliente.calcular_total()
+cliente.mostrar_total()
