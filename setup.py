61a5adbb99b9326d4cb973a65601019e84073ffa
Roberto Chaile <rechaile@gmail.com>
2023-12-13 18:33:07 -0300

    creación del paquete del programa

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="segunda_preentrega_Chaile",
+    version="1.0",
+    description="Este paquete contiene el programa referido a la segunda preentrega de Roberto Chaile para el curso de Python",
+    author="Roberto Ezequiel Chaile",
+    author_email="rechaile@gmail.com",
+    
+)
